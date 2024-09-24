bc21d2323b2850a37d27bdb3046ed3c0a3fad3f5
Jason Polites <jason.polites@gmail.com>
2024-09-23 22:34:50 -0700

    Added js code

--- db.js
+++ db.js
@@ -1,9 +1,18 @@
-const listDBs = async (origin) => {
-  console.log(`Listing DBs from ${origin}...`);
+const listDBs = async (src) => {
+  console.log(`Listing DBs from ${src}...`);
   const databases = await indexedDB.databases();
   for (let i = 0; i < databases.length; i++) {
-    const element =databases[i];
-    console.log(element);
+    const dbRef= databases[i];
+    console.log(dbRef);
+    const req = indexedDB.open(dbRef.name)
+    req.onsuccess = (e) => {
+      console.log(`opened ${dbRef.name}`);
+      db = e.target.result;
+      console.log(db.objectStoreNames);
+    }
+    req.onerror = (event) => {
+      console.log(`failed to open ${db.name} ${event.target.error?.message}`);
+    };
   }
 }
 
